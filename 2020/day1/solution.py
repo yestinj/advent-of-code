35a57e18a48ba85b0f3dedcfeecc80e661c364f5
Yestin Johnson <1382137+yestinj@users.noreply.github.com>
2020-12-01 21:17:27 +0000

    Day 1

---
+++ 2020/day1/solution.py
@@ -0,0 +1,23 @@
+
+def work():
+    input_data = None
+    with open("input.txt") as f:
+        input_data = f.read()
+
+    lines = input_data.split("\n")
+
+    for i in range(len(lines)):
+        for j in range(i+1, len(lines)):
+            for k in range(i+2, len(lines)):
+                if not (lines[i] and lines[j] and lines[k]):
+                    continue
+                one = int(lines[i])
+                two = int(lines[j])
+                three = int(lines[k])
+                if (one + two + three) == 2020:
+                    print('Found: ' + str(one) + " " + str(two) + " " + str(three))
+                    print(one * two * three)
+                    return
+
+if __name__ == "__main__":
+    work()
